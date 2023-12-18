3dfef24c0415f208041d9470425676de9f58e753
ATreeShine <66901031+ATreeShine@users.noreply.github.com>
2023-12-18 16:10:23 +0600

    Update insertionSort.java

--- insertionSort.java
+++ insertionSort.java
@@ -1,24 +1,24 @@
-
 import java.util.Arrays;
 
-public class insertionSort{
-	public static void sort(int unsortedArray[]) {
-		for(int i = 1; i < unsortedArray.length; i++) {
-			int val = unsortedArray[i];
-			int k = i;
-			while(k > 0 && unsortedArray[k-1]>val) {
-				unsortedArray[k] = unsortedArray[k-1];
-				k--;
-				
-			}
-			unsortedArray[k] = val;
-		}
-	}
-	public static void main(String[] args) {
-		int[] randomArray = {1, 3, 8, 2, 4, 6, 2, 3};
-		sort(randomArray);
-		System.out.println(Arrays.toString(randomArray));
+public class InsertionSort {
+    public static void sort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
 
-		
-	}
+            // Move elements of array[0..i-1] that are greater than key
+            // to one position ahead of their current position
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] randomArray = {1, 3, 8, 2, 4, 6, 2, 3};
+        sort(randomArray);
+        System.out.println(Arrays.toString(randomArray));
+    }
 }
